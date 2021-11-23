509537d10c730b7444285f5c82ffe6e20a315f46
Your Name <you@example.com>
2021-11-23 14:32:06 -0700

    test style suggestion

--- test/factiiv-gsn.js
+++ test/factiiv-gsn.js
@@ -6,45 +6,54 @@ const { solidity } = require("ethereum-waffle");
 var expect = chai.expect;
 
 const Web3HttpProvider = require("web3-providers-http");
+const rootAddress = "0x19E7E376E7C213B7E7e7e46cc70A5dD086DAff2A";
+const minimum1 = "101";
+const minimum2 = "102";
 
 //we still use truffle compiled files
 const Factiiv = require("../artifacts/contracts/Factiiv.sol/Factiiv");
-
 chai.use(solidity);
+
 describe("using ethers with OpenGSN", () => {
   let factiiv;
   let web3provider;
   let from;
+
   before(async () => {
     let env = await GsnTestEnvironment.startGsn("localhost");
     // console.log("envir var: ", env);
     const { paymasterAddress, forwarderAddress } = env.contractsDeployment;
     web3provider = new Web3HttpProvider("http://localhost:8545");
     const deploymentProvider = new ethers.providers.Web3Provider(web3provider);
+
     //     root address is variable and can be changed according to needs
     //     root address is assigned the governance role on contract deployment
-    var rootAddress = "0x19E7E376E7C213B7E7e7e46cc70A5dD086DAff2A";
     const factory = new ethers.ContractFactory(
       Factiiv.abi,
       Factiiv.bytecode,
       deploymentProvider.getSigner()
     );
+
     factiiv = await factory.deploy(rootAddress, forwarderAddress);
     await factiiv.deployed();
     const config = {
       paymasterAddress: paymasterAddress,
     };
+
     console.log("paymaster address", paymasterAddress);
     let gsnProvider = RelayProvider.newProvider({
       provider: web3provider,
       config,
     });
+
     await gsnProvider.init();
+
     // The above is the full provider configuration. can use the provider returned by startGsn:
     //     const gsnProvider = env.relayProvider;
     const account = new ethers.Wallet(Buffer.from("1".repeat(64), "hex"));
     gsnProvider.addAccount(account.privateKey);
     from = account.address;
+
     console.log("sending accoutt ::", account.address);
     // gsnProvider is now an rpc provider with GSN support. make it an ethers provider:
     const etherProvider = new ethers.providers.Web3Provider(gsnProvider);
@@ -53,12 +62,15 @@ describe("using ethers with OpenGSN", () => {
 
   describe("Set Minimum Amount", async () => {
     it("Should set Minmum Amount", async () => {
-      await factiiv.setMinimumAmount(Number(100));
+      await factiiv.setMinimumAmount(minimum1);
+      let min1 = await factiiv.minimumAmount();
+      expect(min1.toString()).to.equal(min1);
 
-      await expect(factiiv.setMinimumAmount(Number(100)))
+      await expect(factiiv.setMinimumAmount(minimum2))
         .to.emit(factiiv, "SetMinimum")
-        .withArgs("0x19E7E376E7C213B7E7e7e46cc70A5dD086DAff2A", 100);
-      // expect((await factiiv.minimumAmount()).toNumber()).to.equal(Number(100));
+        .withArgs(rootAddress, minimum2);
+
+      expect((await factiiv.minimumAmount()).toString()).to.equal(minimum2);
     });
   });
 });
